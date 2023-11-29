ee5ae0a1db2e64fd32d1b5e03394f8f136d3f888
Zvi-Erblich <zvierb@gmail.com>
2023-11-29 11:33:59 +0200

    29/11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import wrapper from "./components/wrapper/wrapper";
+import screen from "./components/screen/screen";
+import buttonBox from "./components/buttonBox/buttonBox";
+import button from "./components/button/button";
 
-
-
-function App() {
+const App = () => {
   return (
-    <button>hey</button>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
